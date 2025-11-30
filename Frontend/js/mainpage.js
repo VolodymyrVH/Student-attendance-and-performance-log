d3417cf7cd7cae3e5608bd78c92fa0b7d5031610
soolyshine <anna.krasnobaieva@infiz.khpi.edu.ua>
2025-11-30 18:04:08 +0200

    Add files via upload

--- Frontend/js/mainpage.js
+++ Frontend/js/mainpage.js
@@ -1,1 +1,7 @@
+const menuBtn = document.getElementById("menuBtn");
+        const sideMenu = document.getElementById("sideMenu");
 
+        menuBtn.addEventListener("click", () => {
+            sideMenu.style.display = 
+                sideMenu.style.display === "block" ? "none" : "block";
+        });
